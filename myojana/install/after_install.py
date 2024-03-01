a802caf87df1d44bfac679e86b7942a2994ca21b
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-03-01 11:24:21 +0000

    add logo update

--- myojana/install/after_install.py
+++ myojana/install/after_install.py
@@ -1,5 +1,11 @@
 import frappe
 def update_myojana_logo():
+    # update defult page /app
     query =  """UPDATE tabSingles SET value = "mYojana" WHERE doctype = "Website Settings" AND field = "app_name";"""
+    update_website_logo = """UPDATE tabSingles SET value = "images/mYojana-remove.png" WHERE doctype = "Website Settings" AND field = "app_logo";"""
     frappe.db.sql(query, as_dict=True)
+    frappe.db.sql(update_website_logo, as_dict=True)
+    pass
+
+def update_navbar_setting():
     pass
