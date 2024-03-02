45c4b70f2860b28c0d79d08185832629b896a9b0
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-03-02 14:32:00 +0000

    overwrite frappe defult logo to myojana

--- myojana/install/after_install.py
+++ myojana/install/after_install.py
@@ -1,8 +1,15 @@
 import frappe
+
 def update_myojana_logo():
-    # update defult page /app
     query =  """UPDATE tabSingles SET value = "mYojana" WHERE doctype = "Website Settings" AND field = "app_name";"""
     update_website_logo = """UPDATE tabSingles SET value = "images/mYojana-remove.png" WHERE doctype = "Website Settings" AND field = "app_logo";"""
+    update_website_brand_image = """UPDATE tabSingles SET value = "images/mYojana-remove.png" WHERE doctype = "Website Settings" AND field = "banner_image";"""
+    update_splash_image = """UPDATE tabSingles SET value = "/images/mYojana-remove.png" WHERE doctype = "Website Settings" AND field = "splash_image";"""
+    update_app_logo = """UPDATE tabSingles SET value = "/images/mYojana-remove.png" WHERE doctype = "Navbar Settings" AND field = "app_logo";"""
+    
+    frappe.db.sql(update_website_brand_image, as_dict=True)
+    frappe.db.sql(update_app_logo, as_dict=True)
+    frappe.db.sql(update_splash_image, as_dict=True)
     frappe.db.sql(query, as_dict=True)
     frappe.db.sql(update_website_logo, as_dict=True)
     pass
