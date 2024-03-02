497567593e16c9348320cbce5dc747cdd661ce1f
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-03-02 14:36:56 +0000

    add favicon img

--- myojana/install/after_install.py
+++ myojana/install/after_install.py
@@ -5,6 +5,7 @@ def update_myojana_logo():
     update_website_logo = """UPDATE tabSingles SET value = "images/mYojana-remove.png" WHERE doctype = "Website Settings" AND field = "app_logo";"""
     update_website_brand_image = """UPDATE tabSingles SET value = "images/mYojana-remove.png" WHERE doctype = "Website Settings" AND field = "banner_image";"""
     update_splash_image = """UPDATE tabSingles SET value = "/images/mYojana-remove.png" WHERE doctype = "Website Settings" AND field = "splash_image";"""
+    update_favicon = """UPDATE tabSingles SET value = "/images/mYojana-remove.png" WHERE doctype = "Website Settings" AND field = "favicon";"""
     update_app_logo = """UPDATE tabSingles SET value = "/images/mYojana-remove.png" WHERE doctype = "Navbar Settings" AND field = "app_logo";"""
     
     frappe.db.sql(update_website_brand_image, as_dict=True)
@@ -12,6 +13,7 @@ def update_myojana_logo():
     frappe.db.sql(update_splash_image, as_dict=True)
     frappe.db.sql(query, as_dict=True)
     frappe.db.sql(update_website_logo, as_dict=True)
+    frappe.db.sql(update_favicon, as_dict=True)
     pass
 
 def update_navbar_setting():
