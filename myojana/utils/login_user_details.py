36fe8647919de56f0f1ccdefcd5cfd44b4b7cf28
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-03-10 11:28:10 +0000

    add doctype changes

--- myojana/utils/login_user_details.py
+++ myojana/utils/login_user_details.py
@@ -2,16 +2,16 @@ import frappe
 
 class  LoginUser:
     def get_single_windows():
-        usr = frappe.get_doc("Sipms User", frappe.session.user)
+        usr = frappe.get_doc("Myojana User", frappe.session.user)
         single_window = usr.single_window
         return single_window
 
     # def get_helpdesk():
-    #     usr = frappe.get_doc("Sipms User", frappe.session.user)
+    #     usr = frappe.get_doc("Myojana User", frappe.session.user)
     #     help_desk = usr.help_desk
     #     return help_desk
     
     def get_state():
-        usr = frappe.get_doc("Sipms User", frappe.session.user)
+        usr = frappe.get_doc("Myojana User", frappe.session.user)
         state = usr.state
         return state
