e5537eb0a0baad99343de7f80528a33be2590793
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-03-11 12:25:35 +0000

    solve issues

--- myojana/myojana/doctype/beneficiary_profiling/beneficiary_profiling.py
+++ myojana/myojana/doctype/beneficiary_profiling/beneficiary_profiling.py
@@ -102,7 +102,7 @@ class BeneficiaryProfiling(Document):
 			# 		return
 	def after_insert(self):
 		print("Ben[after_insert]")
-		if not self.single_window:
+		if not self.single_window and "Administrator" not in frappe.get_roles(frappe.session.user):
 			single_window = LoginUser.get_single_windows()
 			self.single_window = single_window
 			frappe.db.set_value('Beneficiary Profiling', self.name, 'single_window', single_window, update_modified=False)
