29a4bcaa67b9d055ff9d254af7017648361c65ed
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-03-18 16:46:52 +0530

    add role permission doctype

--- myojana/myojana/doctype/role_permission/role_permission.js
+++ myojana/myojana/doctype/role_permission/role_permission.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2024, dhwaniris and contributors
 // For license information, please see license.txt
+async function get_field_list (frm) {
+    frappe.call({
+        method: "myojana.rule_engine.apis.get_meta_api.get_field_lists",
+        args: {
+            doctype_name: "Beneficiary Profiling",
+            field_types: ['Link']
+        },
+        callback: function (response) {
+            // Handle the response
+            if (response.message) {
+                // frm.fields_dict.scheme_table.grid.open_grid_row.fields_dict.name_of_the_scheme._data = ops
+                frm.set_df_property('filter_key', 'options', response.message)
+                // return response.message
+            } else {
+                console.error("API call failed");
+            }
+        }
+    });
 
-// frappe.ui.form.on("Role Permission", {
-// 	refresh(frm) {
-
-// 	},
-// });
+}
+frappe.ui.form.on("Role Permission", {
+	async refresh(frm) {
+        await get_field_list(frm)
+	},
+});
