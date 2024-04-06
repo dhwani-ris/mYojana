0ca67628a0ab55943893a94cba459d810814e79a
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-04-06 11:37:39 +0530

    added myojana setting in

--- myojana/myojana/doctype/myojana_settings/myojana_settings.js
+++ myojana/myojana/doctype/myojana_settings/myojana_settings.js
@@ -1,16 +1,32 @@
 // Copyright (c) 2024, dhwaniris and contributors
 // For license information, please see license.txt
-const get_doctype_list = async (frm) => {
+// const get_doctype_list = async () => {
+//     let list = await callAPI({
+//       method: 'myojana.apis.get_doctype_meta.get_allowed_doctype',
+//       freeze: true,
+//       args: {
+//         "doctype_name":"Beneficiary Profiling",
+//         "field_types":"Link"
+//       },
+//       freeze_message: __("Getting Fields..."),
+//     })
+//     cur_frm.cur_grid.grid_form.fields_dict.doctypes._data = list
+//     // frm.fields_dict.select_doctypes.grid.open_grid_row.fields_dict.doctypes._data = list;
+//     return list;
+//   }
+  const get_fields_list = async (doctype) => {
     let list = await callAPI({
       method: 'myojana.apis.get_doctype_meta.get_allowed_doctype',
       freeze: true,
       args: {
         "doctype_name":"Beneficiary Profiling",
-        "field_types":"Link"
+        "field_types":"Link",
+        "options": doctype
       },
       freeze_message: __("Getting Fields..."),
     })
-    cur_frm.cur_grid.grid_form.fields_dict.doctypes._data = list
+    // list = list.map((e)=>{e.label = list.label, e.value = list.label})
+    cur_frm.cur_grid.grid_form.fields_dict.field_name._data = list
     // frm.fields_dict.select_doctypes.grid.open_grid_row.fields_dict.doctypes._data = list;
     return list;
   }
@@ -22,9 +38,16 @@ frappe.ui.form.on("mYojana Settings", {
 
 frappe.ui.form.on('Setting Doctype Child', {
     form_render: async function (frm, cdt, cdn) {
-        await get_doctype_list()
+        let row = frappe.get_doc(cdt, cdn);
+        row.doctypes &&await get_fields_list(row.doctypes)
+        
+        
     },
     select_doctypes_add: async function (frm, cdt, cdn) {
       console.log("hello everyone")
+    },
+    doctypes: async function(frm , cdt , cdn){
+        let row = frappe.get_doc(cdt, cdn);
+        row.doctypes &&await get_fields_list(row.doctypes)
     }
   })
