0441cbd8c382e9ba7a73aeb258f9621aafb72ed8
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2024-05-08 18:28:36 +0530

    add data export buttion

--- myojana/myojana/doctype/report_list/report_list.js
+++ myojana/myojana/doctype/report_list/report_list.js
@@ -1,6 +1,9 @@
 frappe.ui.form.on("Report List", {
     refresh(frm) {
-        fetchAndRenderData(frm);
+        if (!frm.is_new() == 1) {
+            fetchAndRenderData(frm);
+        }
+        DataExportButton(frm)
     },
 });
 
@@ -36,13 +39,47 @@ function renderDataTable(e) {
     let datatable = new frappe.DataTable("#list", {
         columns: columns,
         data: data,
-        // filters: [
-        //     { name: 'column1', label: __('Column 1'), fieldtype: 'Data', },
-        //     { name: 'column2', label: __('Column 2'), fieldtype: 'Data', },
-        //     // Add more filters as needed
-        // ],
-        // layout: 'fluid',
+        paging: true, // Enable pagination
+        pageLength: 10, // Set number of rows per page
+        lengthChange: false, // Hide length change dropdown
+        info: false, // Hide table information summary
+        searching: false, // Disable search
+        ordering: false,
+        pagingType: 'simple_numbers'
     });
-
     datatable.refresh();
+    // new datatable('#example', {
+    //     pagingType: 'simple_numbers'
+    // });
+
+}
+
+function DataExportButton(frm) {
+    frm.add_custom_button(__('Export'), function () {
+        frappe.call({
+            method: "myojana.sva_report.controllers.get_report_data.execute",
+            args: {
+                doc: frm.doc.name,
+            },
+            callback: function (response) {
+                if (response.message) {
+                    renderDataTable(response.message);
+                    downloadFile(response.message.data, "exported_data.csv", "text/csv");
+                } else {
+                    console.error("Error fetching data from API.");
+                }
+            }
+        });
+    });
+}
+function downloadFile(data, filename, type) {
+    if (typeof data !== 'string') {
+        data = JSON.stringify(data);
+    }
+    var blob = new Blob([data], { type: type });
+    var link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
 }
