172e2897ccb6909573ecd9f065cbe062f30cf015
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-05-10 17:21:16 +0530

    add repoart template

---
+++ myojana/myojana/report/apply_at_least_1_schemes/apply_at_least_1_schemes.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2024, dhwaniris and contributors
+// For license information, please see license.txt
+var filters = [
+	{
+		"fieldname": "from_date",
+		"fieldtype": "Date",
+		"label": "From Date",
+	},
+	{
+		"fieldname": "to_date",
+		"fieldtype": "Date",
+		"label": "To Date"
+	},
+];
+frappe.query_reports["Apply at least 1 schemes"] = {
+	filters: filters
+};
---
+++ myojana/myojana/report/apply_for_more_than_3_schemes/apply_for_more_than_3_schemes.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, dhwaniris and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Apply for more than 3 schemes"] = {
+	"filters": [
+
+	]
+};
