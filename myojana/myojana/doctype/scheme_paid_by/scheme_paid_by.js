b0cea61185da695c1f892436eceb322a8d42aa60
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-05-16 10:55:33 +0530

    add master of childtable

---
+++ myojana/myojana/doctype/scheme_paid_by/scheme_paid_by.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, dhwaniris and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Scheme Paid by", {
+// 	refresh(frm) {
+
+// 	},
+// });
