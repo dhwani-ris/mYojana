aae137bc3eb0d9f494c6747d0ce45319632fdc7f
Abhishek kumar <abhishek.suvaidyam@gmail.com>
2024-05-27 10:26:34 +0530

    new repoart is added

---
+++ myojana/myojana/report/beneficiaries_who_have_received_benefits_of_at_least_1_scheme_in_each_category/beneficiaries_who_have_received_benefits_of_at_least_1_scheme_in_each_category.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2024, dhwaniris and contributors
+# For license information, please see license.txt
+
+# import frappe
+
+
+import frappe
+from myojana.utils.report_filter import ReportFilter
+
+def execute(filters=None):
+	# frappe.errprint(filters)
+	columns = [
+		{
+		"fieldname":"n",
+		"label":"No. of beneficiaries",
+		"fieldtype":"int",
+		"width":200
+		},
+		{
+		"fieldname":"count",
+		"label":"No. distinct beneficiaries",
+		"fieldtype":"int",
+		"width":200
+		}
+	]
+	condition_str = ReportFilter.set_report_filters(filters, 'creation', True)
+	if condition_str:
+		condition_str = f"AND {condition_str}"
+	else:
+		condition_str = ""
+
+	distinct_milestone = frappe.db.sql("select count(distinct milestone) as count from `tabScheme` where enabled = '1'", as_dict=True)
+	mcount = distinct_milestone[0].count if len(distinct_milestone) > 0 else 0
+	query = f"""
+		select 
+			*
+		from
+			(
+				select 
+					'Number of beneficiaries' as n,
+					(count(distinct milestone)) = {mcount} as count
+				from
+					`tabScheme Child`
+				where 
+					parenttype = 'Beneficiary Profiling' and parentfield = 'scheme_table' 
+				group by parent
+			) as t
+		where t.count = 1 {condition_str} 
+	"""
+	ben_data = frappe.db.sql(query, as_dict=True)
+	return columns, ben_data
