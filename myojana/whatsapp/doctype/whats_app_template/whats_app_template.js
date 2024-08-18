9aca50b0bf41e9d00d5ab8ee529528df653caa72
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-08-18 12:52:04 +0530

    add html to img binary convert

--- myojana/whatsapp/doctype/whats_app_template/whats_app_template.js
+++ myojana/whatsapp/doctype/whats_app_template/whats_app_template.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2024, dhwaniris and contributors
 // For license information, please see license.txt
+const wait = (seconds)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve();
+        },seconds*1000)
+    })    
+}
+frappe.ui.form.on("Whats App Template", {
+	refresh(frm) {
 
-// frappe.ui.form.on("Whats App Template", {
-// 	refresh(frm) {
-
-// 	},
-// });
+	},
+    html:(frm)=>{
+        frappe.call({
+            method: "myojana.whatsapp.utils.htmltoimg.preview_image",
+            args: {
+                doctype:frm.doc.variable_reference_doctype,
+                doc:frm.doc.select_reference_variable,
+                template:frm.doc.html
+            }
+        }).then(async(res)=>{
+            document.getElementById("preview_html").innerHTML = `<img src="data:image/png;base64,${res.message}" />`;
+            
+        }).catch(err=>{
+            console.log("Error",err);
+        })
+    }
+});
