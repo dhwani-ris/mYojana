cee3f66693c2a9cec7b0ecbe009c12e7d02ca971
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-10-09 12:44:49 +0000

    add add a report for agrasar nad number card

---
+++ myojana/myojana/report/number_of_unique_beneficiaries_(_government_schemes_+documentation_)/number_of_unique_beneficiaries_(_government_schemes_+documentation_).py
@@ -0,0 +1,46 @@
+# Copyright (c) 2023, suvaidyam and contributors
+# For license information, please see license.txt
+
+import frappe
+from myojana.utils.report_filter import ReportFilter
+
+
+def execute(filters=None):
+    columns = [
+        {
+            "fieldname": "milestone_category",
+            "label": "Support Category",
+            "fieldtype": "Data",
+            "width": 200,
+            
+        },
+        {
+            "fieldname": "total_demands",
+            "label": "Total Unique Demands",
+            "fieldtype": "Data",
+            "width": 130,
+        }
+    ]
+
+    condition_str = ReportFilter.set_report_filters(filters, 'date_of_visit', True , 'ben_table')
+    if condition_str:
+        condition_str = f"AND {condition_str}"
+    else:
+        condition_str = ""
+
+    sql_query = f"""
+    SELECT
+        milestone_category,
+        COUNT(DISTINCT(_sc.parent)) as total_demands
+    FROM
+        `tabScheme Child` as _sc
+    INNER JOIN `tabBeneficiary Profiling` as ben_table on (ben_table.name =  _sc.parent and _sc.parenttype ='Beneficiary Profiling')
+    WHERE
+        1=1 {condition_str} AND milestone_category IN ("Documentation","Government schemes")
+    GROUP BY
+        milestone_category;
+"""
+
+
+    data = frappe.db.sql(sql_query, as_dict=True)
+    return columns, data
