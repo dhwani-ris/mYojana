af6b37eea439faed7ad6f1d146743d6bacf8665d
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-10-13 19:48:25 +0530

    add changes

--- myojana/myojana/report/place_of_origin/place_of_origin.py
+++ myojana/myojana/report/place_of_origin/place_of_origin.py
@@ -11,13 +11,19 @@ def execute(filters=None):
             "fieldname": "state",
             "label": "State",
             "fieldtype": "Data",
-            "width": 400
+            "width": 200
         },
         {
             "fieldname": "district",
             "label": "District",
             "fieldtype": "Data",
-            "width": 400
+            "width": 200
+        },
+        {
+            "fieldname": "block",
+            "label": "Block",
+            "fieldtype": "Data",
+            "width": 200
         },
         {
             "fieldname": "count",
@@ -36,14 +42,16 @@ def execute(filters=None):
         SELECT
             COALESCE(NULLIF(s.state_name, ''), 'Unknown') AS state,
             COALESCE(NULLIF(d.district_name, ''), 'Unknown') AS district,
+            COALESCE(NULLIF(bl.block_name, ''), 'Unknown') AS block,
             COUNT(b.name) AS count
         FROM
             `tabBeneficiary Profiling` AS b
             LEFT JOIN tabState AS s ON b.state_of_origin = s.name
             LEFT JOIN tabDistrict AS d ON b.district_of_origin = d.name
+            LEFT JOIN tabBlock as bl ON b.ward = bl.name
         WHERE {condition_str}
         GROUP BY
-            COALESCE(NULLIF(b.state_of_origin, ''), 'Unknown'), COALESCE(NULLIF(b.district_of_origin, ''), 'Unknown')
+            COALESCE(NULLIF(b.state_of_origin, ''), 'Unknown'), COALESCE(NULLIF(b.district_of_origin, ''), 'Unknown') , COALESCE(NULLIF(b.ward, ''), 'Unknown')
         ORDER BY
             COALESCE(NULLIF(b.state_of_origin, ''), 'Unknown'), COALESCE(NULLIF(b.district_of_origin, ''), 'Unknown');
     """
