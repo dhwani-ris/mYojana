df5ba10c381dfa5462ba6a9be390c6424094b8c8
Abhishek Kumar <abhishek.suvaidyam@gmail.com>
2024-10-14 07:05:34 +0530

    add changes

---
+++ myojana/myojana/report/tracking_of_occupation_of_beneficiaries,_segregated_by_caste/tracking_of_occupation_of_beneficiaries,_segregated_by_caste.py
@@ -0,0 +1,49 @@
+import frappe
+from myojana.utils.report_filter import ReportFilter
+
+def execute(filters=None):
+    columns = [
+        {
+            "fieldname": "caste_category",
+            "label": "Caste",
+            "fieldtype": "Link",
+            "options": "Caste category",
+            "width": 200
+        },
+        {
+            "fieldname": "occupation",
+            "label": "Occupation",
+            "fieldtype": "Link",
+            "options": "Occupation",
+            "width": 300
+        },
+        {
+            "fieldname": "count",
+            "label": "Count",
+            "fieldtype": "int",
+            "width": 200
+        }
+    ]
+
+    condition_str = ReportFilter.set_report_filters(filters, 'date_of_visit', True, 'ben')
+    if condition_str:
+        condition_str = f"AND {condition_str}"
+    else:
+        condition_str = ""
+
+    sql_query = f"""
+        SELECT
+			ben.caste_category as caste_category,
+            ben.current_occupation as occupation,
+            COUNT(ben.name) as count
+        FROM
+            `tabBeneficiary Profiling` as ben
+        WHERE
+        1=1 {condition_str}
+        GROUP BY
+            ben.caste_category
+		ORDER BY ben.caste_category ASC, ben.current_occupation ASC
+    """
+
+    data = frappe.db.sql(sql_query, as_dict=True)
+    return columns, data
