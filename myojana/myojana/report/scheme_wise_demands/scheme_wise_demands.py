edf1c2ed30c2f8083c50d94bf5db1885111d3520
rsuvaidyam <rkrahul00011@gmail.com>
2025-03-25 16:27:39 +0530

    report:- scheme wise demands change filter follow_up_date -> follow_up_date

--- myojana/myojana/report/scheme_wise_demands/scheme_wise_demands.py
+++ myojana/myojana/report/scheme_wise_demands/scheme_wise_demands.py
@@ -65,36 +65,38 @@ def execute(filters=None):
             "width": 130,
         },
     ]
-
-    condition_str = ReportFilter.set_report_filters(filters, 'date_of_visit', True , 'ben_table')
+    user = frappe.session.user
+    condition_str = ReportFilter.set_report_filters(filters, 'follow_up_date', True , '_fuc')
     if condition_str:
         condition_str = f"AND {condition_str}"
     else:
         condition_str = ""
-
+    # check user role
+    if 'Sub-Centre' in frappe.get_roles(user) and 'Administrator' not in frappe.get_roles(user):
+        condition_str += f" AND _sc.modified_by = '{user}'"
     sql_query = f"""
-    SELECT
-        _sc.modified_by as user,
-        _sc.scheme,
-        _sc.milestone,
-        SUM(CASE WHEN (_sc.status = 'Open') THEN 1 ELSE 0 END) as open_demands,
-        SUM(CASE WHEN (_sc.status = 'Completed') THEN 1 ELSE 0 END) as completed_demands,
-        SUM(CASE WHEN (_sc.status = 'Closed') THEN 1 ELSE 0 END) as closed_demands,
-        SUM(CASE WHEN (_sc.status = 'Under process') THEN 1 ELSE 0 END) as submitted_demands,
-        SUM(CASE WHEN (_sc.status = 'Rejected') THEN 1 ELSE 0 END) as rejected_demands,
-        COUNT(_sc.status) as total_demands
-    FROM
-        `tabScheme Child` as _sc
-    INNER JOIN `tabBeneficiary Profiling` as ben_table 
-        ON (ben_table.name = _sc.parent AND _sc.parenttype = 'Beneficiary Profiling')
-    WHERE
-        1 = 1 {condition_str}
-    GROUP BY
-        _sc.scheme,
-        _sc.modified_by,
-        _sc.milestone;
-"""
-
-
+        SELECT
+            _sc.modified_by as user,
+            _sc.scheme,
+            _sc.milestone,
+            SUM(CASE WHEN (_sc.status = 'Open') THEN 1 ELSE 0 END) as open_demands,
+            SUM(CASE WHEN (_sc.status = 'Completed') THEN 1 ELSE 0 END) as completed_demands,
+            SUM(CASE WHEN (_sc.status = 'Closed') THEN 1 ELSE 0 END) as closed_demands,
+            SUM(CASE WHEN (_sc.status = 'Under process') THEN 1 ELSE 0 END) as submitted_demands,
+            SUM(CASE WHEN (_sc.status = 'Rejected') THEN 1 ELSE 0 END) as rejected_demands,
+            COUNT(_sc.status) as total_demands
+        FROM
+            `tabScheme Child` as _sc
+        LEFT JOIN `tabFollow Up Child` as _fuc
+            ON (_fuc.name_of_the_scheme = _sc.name_of_the_scheme AND _fuc.parenttype = 'Beneficiary Profiling')
+        INNER JOIN `tabBeneficiary Profiling` as ben_table 
+            ON (ben_table.name = _sc.parent)
+        WHERE
+            1 = 1 {condition_str}
+        GROUP BY
+            _sc.scheme,
+            _sc.modified_by,
+            _sc.milestone;
+    """
     data = frappe.db.sql(sql_query, as_dict=True)
     return columns, data
